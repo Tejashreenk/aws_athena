4fbb7a478cd77e773200650367903093269407c2
Tejashree <tejashri@uw.edu>
2024-08-24 23:40:58 -0700

    merged changed code from lambda, added standalone query code

--- lambda_aws.py
+++ lambda_aws.py
@@ -1,7 +1,7 @@
 import boto3
 import json
-import time
 import re
+import time
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 
 def lambda_handler(payload, context):
@@ -16,19 +16,17 @@ def lambda_handler(payload, context):
 
     # Initialize Athena client
     athena_client = boto3.client('athena')
-
+    
     event = json.loads(payload['body'])
     print(f"payload {event}")
-
     # Initialize parameters
     database = event['database']
     table_name = event['table_name']
     client = event['client']
     output_bucket = 's3://llm-output-bucket/'
-    query_ans_arr = event.get('query_ans_arr', [])
-
+    
     # Create SQL_Answer_Agent instance
-    sql_agent = SQL_Answer_Agent(bedrock_client, athena_client, database, table_name, output_bucket, query_ans_arr)
+    sql_agent = SQL_Answer_Agent(bedrock_client, athena_client, database, table_name, output_bucket)
     
     # Get and set the schema
     sql_agent.get_set_db_schema()
@@ -43,20 +41,19 @@ def lambda_handler(payload, context):
     
     return {
         # 'statusCode': 200,
-        'body': answer
+        'answer': answer
     }
 
 
 
 class SQL_Answer_Agent:
-    def __init__(self, bedrock_client, athena_client, database,table_name, output_bucket, query_ans_arr) -> None:
+    def __init__(self, bedrock_client, athena_client, database,table_name, output_bucket) -> None:
         # self.client = anthropic_client
         self.bedrock_client = bedrock_client
         self.athena_client = athena_client
         self.database = database
         self.output_bucket = output_bucket
         self.table_name = table_name
-        self.query_ans_arr = query_ans_arr
         self.schema = None
 
     def set_prompt(self, question, prompt=None):
@@ -92,13 +89,14 @@ class SQL_Answer_Agent:
         response = response_body.get('content')[0].get('text')
         # print(response)
         return response
-    
+        
+           
     def get_set_db_schema(self):
         try:
             query = f"describe {self.table_name};"
             query_result = self.execute_sql_query(query)
             self.schema = query_result
-            # print(f"self.schema:{self.schema}")
+            print(f"self.schema:{self.schema}")
             return True
         except (NoCredentialsError, PartialCredentialsError) as e:
             print(f"Credentials error: {e}")
@@ -107,7 +105,7 @@ class SQL_Answer_Agent:
             print(f"Error fetching schema: {e}")
             return False
 
-    def execute_sql_query(self, query) -> bool:
+    def execute_sql_query(self, query):
         if query:
             try:
                 response = self.athena_client.start_query_execution(
@@ -121,7 +119,7 @@ class SQL_Answer_Agent:
                 # print(response)
                 while True:
                     result = self.athena_client.get_query_execution(QueryExecutionId=query_execution_id)
-                    # print(result)
+                    print(result)
                     status = result['QueryExecution']['Status']['State']
                     if status == 'SUCCEEDED':
                         break
@@ -138,67 +136,39 @@ class SQL_Answer_Agent:
                 return False
         else:
             return False
-        
-    def create_standalone_query(self, query_ans_arr):
-        if len(query_ans_arr) > 0:
-            prompt = f'''Create standalone query from the history:{query_ans_arr}. 
-            Write query in between tags like <STA></STA>.'''
-        
-            llm_response = self.get_llm_response(prompt)
-            query_standalone = self.extract_standalone_query(llm_response)
-            return query_standalone
 
-
-    def get_answer(self, query, client, prompt=None):
-        self.query_ans_arr.append(query)
-        if query:
-            query = self.create_standalone_query(self.query_ans_arr)
-
-        if prompt:
-            prompt = prompt
-        else:
-            prompt = f'''Use the schema for table {self.table_name} mentioned below to prepare query. 
-            Schema - {self.schema}. Please provide the SQL query for this question:{query} and for client {client} '''
-            
-        # print(f"prompt: {prompt}") 
-        llm_response = self.get_llm_response(prompt)
-        sql_query = self.extract_sql(llm_response)
-        print(f"llm_response: {llm_response}") 
-        print(f"sql_query: {sql_query}") 
-        sql_response = self.execute_sql_query(sql_query)
-        response_summary = self.summarize_sql_response(query, f'<data> {sql_response}')
-
-        return response_summary
-
-    def summarize_sql_response(self, query, sql_response):
-        prompt = f"Analyze the data mentioned below and respond only with the analysis based on the given Question {query}  : {sql_response}"
-        # prompt = f"Summarize: {sql_response}"
-        response = self.summary_llm_agent(prompt)
-        return response
 
     def extract_sql(self, text):
         pattern = r'<SQL>(.*?)</SQL>'
         matches = re.findall(pattern, text, re.DOTALL)
         cleaned_matches = [match.strip() for match in matches]
-        if cleaned_matches:
-            cleaned_match = cleaned_matches[0]
-            # Replace newline characters with spaces
-            return cleaned_match.replace('\n', ' ')
-        else:
-            return "" #Future: Loop over the queries        return cleaned_matches[0] #Future: Loop over the queries
+        return cleaned_matches
         
-    def extract_standalone_query(self, text):
-        pattern = r'<STA>(.*?)</STA>'
-        matches = re.findall(pattern, text, re.DOTALL)
-        cleaned_matches = [match.strip() for match in matches]
-        if cleaned_matches:
-            cleaned_match = cleaned_matches[0]
-            # Replace newline characters with spaces
-            return cleaned_match.replace('\n', ' ')
+    def get_answer(self, query, client, prompt=None):
+        if prompt:
+            prompt = prompt
         else:
-            return "" #Future: Loop over the queries        return cleaned_matches[0] #Future: Loop over the queries
-        
-    
+            prompt =f'''Use the schema for table {self.table_name} mentioned below to prepare query. Schema - {self.schema}. Please provide the SQL query for this question:{query} and for client {client} '''
+            
+            # f"Use the schema {self.schema} for table {self.table_name} and respond ONLY with an SQL query to answer the question: {query}"
+           
+        # print(f"prompt: {prompt}") 
+        sql_query = self.get_llm_response(prompt)
+        print(f"\nSQL QUERY from LLM : {sql_query}") 
+        final_sql = self.extract_sql(sql_query)
+        sql_response = self.execute_sql_query(final_sql[0])
+        print(f"response: {sql_response}")
+        response_summary = self.summarize_sql_response(query, f'<data> {sql_response}')
+
+        return response_summary
+
+    def summarize_sql_response(self,query, sql_response):
+        # p = f'''Specifically, I'm interested in context of asked question. Data mentioned below is the response from Athena for question {query}.  {sql_response}'''
+        # print(p)
+        prompt = f"Analyze the data mentioned below and respond only with the analysis based on the given Question {query}  : {sql_response}"
+        response = self.summary_llm_agent(prompt)
+        return response
+
     def summary_llm_agent(self, content, modelId = "anthropic.claude-3-sonnet-20240229-v1:0"):
         # "anthropic.claude-3-sonnet-20240229-v1:0"
         input = {
